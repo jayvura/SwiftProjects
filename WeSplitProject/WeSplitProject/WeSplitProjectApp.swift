06dc8990790bcbd8db82c8399145826f0151fda2
Jayden <jdecambre10@gmail.com>
2025-03-07 18:46:49 -0500

    Initial commit for Swift Project 1

---
+++ WeSplitProject/WeSplitProject/WeSplitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitProjectApp.swift
+//  WeSplitProject
+//
+//  Created by Jayden on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplitProject/WeSplitProjectTests/WeSplitProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitProjectTests.swift
+//  WeSplitProjectTests
+//
+//  Created by Jayden on 3/7/25.
+//
+
+import Testing
+@testable import WeSplitProject
+
+struct WeSplitProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
